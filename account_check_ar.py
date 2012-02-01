00e81312e730bd1c4c1f01175a9ff6790c5f7ade
Bruno M. Villasanti <bvillasanti@thymbra.com>
2012-02-01 00:10:24 -0300

    Pyflakes fixes

--- account_check_ar.py
+++ account_check_ar.py
@@ -1,10 +1,8 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.model import ModelWorkflow, ModelView, ModelSQL, fields
-from trytond.tools import safe_eval, datetime_strftime
-from trytond.transaction import Transaction
 from decimal import Decimal
-from trytond.pyson import Eval, Not, In, Equal
+from trytond.pyson import Eval, Not, In
 
 _STATES = {
     'readonly': In(Eval('state'), ['posted']),
