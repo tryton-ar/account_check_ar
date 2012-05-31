468216ed7980b574d709c6d80aae08a2f3c0bffb
Sebastián Marró <smarro@thymbra.com>
2012-05-31 02:01:59 -0300

    Update to Tryton 2.4

--- __tryton__.py
+++ __tryton__.py
@@ -2,13 +2,13 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Account Check Argentina',
-    'name_es_ES': 'Cartera de Cheques para Argentina',
-    'version': '2.2.0',
+    'name_es_AR': 'Cartera de Cheques para Argentina',
+    'version': '2.4.0',
     'author': 'Thymbra - Torre de Hanoi',
     'email': 'info@thymbra.com',
     'website': 'http://thymbra.com/',
     'description': '''Account Check for Argentina''',
-    'description_es_ES': '''Manejo de Cartera de Cheques para Argentina''',
+    'description_es_AR': '''Manejo de Cartera de Cheques para Argentina''',
     'depends': [
         'account',
         'account_invoice',
@@ -18,9 +18,8 @@
         'account_check_ar.xml',
         'account_voucher_ar.xml',
         'journal.xml',
-        'workflow.xml'
     ],
     'translation': [
-        'es_ES.po',
+        'es_AR.po',
     ],
 }
--- account_check_ar.py
+++ account_check_ar.py
@@ -1,90 +1,170 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-from trytond.model import ModelWorkflow, ModelView, ModelSQL, fields
+from trytond.model import Workflow, ModelView, ModelSQL, fields
 from decimal import Decimal
 from trytond.pyson import Eval, Not, In
 from trytond.pool import Pool
 
 _STATES = {
-    'readonly': In(Eval('state'), ['posted']),
+    'readonly': Eval('state') != 'draft',
 }
+_DEPENDS = ['state']
 
-
-class AccountIssuedCheck(ModelSQL, ModelView):
+class AccountIssuedCheck(Workflow, ModelSQL, ModelView):
     'Account Issued Check'
     _name = 'account.issued.check'
     _description = __doc__
 
-    name = fields.Char('Number')
-    amount = fields.Float('Amount')
-    date_out = fields.Date('Date Out')
-    date = fields.Date('Date')
-    debit_date = fields.Date('Debit Date')
-    receiving_party_id = fields.Many2One('party.party', 'Receiving Party')
-    on_order = fields.Char('On Order')
-    signatory = fields.Char('Signatory')
+    name = fields.Char('Number', states=_STATES, depends=_DEPENDS)
+    amount = fields.Numeric('Amount', digits=(16, 2), states=_STATES, 
+        depends=_DEPENDS)
+    date_out = fields.Date('Date Out', states=_STATES, depends=_DEPENDS)
+    date = fields.Date('Date', states=_STATES, depends=_DEPENDS)
+    debit_date = fields.Date('Debit Date', depends=_DEPENDS,
+        states={'invisible': Eval('state') != 'debited'})
+    receiving_party = fields.Many2One('party.party', 'Receiving Party', 
+        depends=_DEPENDS, states={'invisible': Eval('state') == 'draft'})
+    on_order = fields.Char('On Order', states=_STATES, depends=_DEPENDS)
+    signatory = fields.Char('Signatory', states=_STATES, depends=_DEPENDS)
     clearing = fields.Selection([
         ('24', '24 hs'),
         ('48', '48 hs'),
         ('72', '72 hs'),
-        ], 'Clearing')
-    origin = fields.Char('Origin')
-    voucher_id = fields.Many2One('account.voucher', 'Voucher')
-    issued = fields.Boolean('Issued')
+        ], 'Clearing', states=_STATES, depends=_DEPENDS)
+    origin = fields.Char('Origin', states=_STATES, depends=_DEPENDS)
+    voucher = fields.Many2One('account.voucher', 'Voucher', depends=_DEPENDS,
+        states={'invisible': Eval('state') == 'draft'})
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('issued', 'Issued'),
+        ('debited', 'Debited'),
+        ], 'State', readonly=True)
+
+    def __init__(self):
+        super( AccountIssuedCheck, self).__init__()
+        self._transitions |= set((
+                ('draft', 'issued'),
+                ('issued', 'debited'),
+                ))    
+        self._buttons.update({
+                'issued': {
+                    'invisible': Eval('state') != 'draft',
+                    },
+                'debited': {
+                    'invisible': Eval('state') != 'issued',
+                    },
+                })    
+
+    def default_date_out(self):
+        date_obj = Pool().get('ir.date')
+        return date_obj.today()
+
+    def default_state(self):
+        return 'draft'
+
+    @Workflow.transition('issued')
+    def issued(self, ids):
+        pass
+        
+    @ModelView.button
+    @Workflow.transition('debited')
+    def debited(self, ids):
+        pass
 
 AccountIssuedCheck()
 
 
-class AccountThirdCheck(ModelWorkflow, ModelSQL, ModelView):
+class AccountThirdCheck(Workflow, ModelSQL, ModelView):
     'Account Third Check'
     _name = 'account.third.check'
     _description = __doc__
 
-    name = fields.Char('Number')
-    amount = fields.Float('Amount')
-    date_in = fields.Date('Date')
-    date = fields.Date('Date')
-    date_out = fields.Date('Date Out')
-    debit_date = fields.Date('Debit Date')
-    source_party_id = fields.Many2One('party.party', 'Source Party')
-    destiny_party_id = fields.Many2One('party.party', 'Destiny Party')
-    on_order = fields.Char('On Order')
-    signatory = fields.Char('Signatory')
+    name = fields.Char('Number', states=_STATES, depends=_DEPENDS)
+    amount = fields.Numeric('Amount', digits=(16, 2), states=_STATES, 
+        depends=_DEPENDS)
+    date_in = fields.Date('Date In', states=_STATES, depends=_DEPENDS)
+    date = fields.Date('Date', states=_STATES, depends=_DEPENDS)
+    date_out = fields.Date('Date Out', depends=_DEPENDS, readonly=True,
+        states={'invisible': In(Eval('state'), ['draft', 'held'])})
+    debit_date = fields.Date('Debit Date', depends=_DEPENDS, readonly=True,
+        states={'invisible': In(Eval('state'), ['draft', 'held'])})
+    source_party = fields.Many2One('party.party', 'Source Party', 
+        depends=_DEPENDS, readonly=True,
+        states={'invisible': Eval('state') == 'draft'})
+    destiny_party = fields.Many2One('party.party', 'Destiny Party', 
+        depends=_DEPENDS, readonly=True,
+        states={'invisible': Eval('state') != 'delivered'})
+    on_order = fields.Char('On Order', states=_STATES, depends=_DEPENDS)
+    signatory = fields.Char('Signatory', states=_STATES, depends=_DEPENDS)
     state = fields.Selection([
         ('draft', 'Draft'),
-        ('C', 'Cartera'),
+        ('held', 'Held'),
         ('deposited', 'Deposited'),
         ('delivered', 'Delivered'),
         ('rejected', 'Rejected'),
         ], 'State', readonly=True)
-    vat = fields.Char('Vat')
+    vat = fields.Char('Vat', states=_STATES, depends=_DEPENDS)
     clearing = fields.Selection([
         ('24', '24 hs'),
         ('48', '48 hs'),
         ('72', '72 hs'),
-        ], 'Clearing')
-    origin = fields.Char('Origin')
-    voucher_id = fields.Many2One('account.voucher', 'Voucher')
-    issued = fields.Boolean('Issued')
-    reject_debit_note = fields.Many2One('account.invoice', 'Debit Note')  # TODO
+        ], 'Clearing', states=_STATES, depends=_DEPENDS)
+    origin = fields.Char('Origin', states=_STATES, depends=_DEPENDS)
+    voucher_in = fields.Many2One('account.voucher', 'Origin Voucher', depends=_DEPENDS, 
+        readonly=True, states={'invisible': Eval('state') == 'draft'})
+    voucher_out = fields.Many2One('account.voucher', 'Target Voucher', depends=_DEPENDS, 
+        readonly=True, states={'invisible': Eval('state') != 'delivered'})
+    reject_debit_note = fields.Many2One('account.invoice', 'Debit Note', 
+        depends=_DEPENDS, readonly=True)  # TODO
 
-    def wkf_draft(self, check_id):
-        self.write(check_id.id, {'state': 'draft'})
-        return True
+    def __init__(self):
+        super(AccountThirdCheck, self).__init__()
+        self._transitions |= set((
+                ('draft', 'held'),
+                ('held', 'deposited'),
+                ('held', 'delivered'),
+                ('deposited', 'rejected'),
+                ('delivered', 'rejected'),
+                ))    
+        self._buttons.update({
+                'held': {
+                    'invisible': Eval('state') != 'draft',
+                    },
+                'deposited': {
+                    'invisible': Eval('state') != 'held',
+                    },
+                'delivered': {
+                    'invisible': Eval('state') != 'held',
+                    },
+                'rejected': {
+                    'invisible': ~Eval('state').in_(['deposited', 'delivered']),
+                    },
+                })    
 
-    def wkf_cartera(self, check_id):
-        self.write(check_id.id, {'state': 'C'})
-        return True
-
-    def wkf_deposited(self, check_id):
-        return True
-
-    def wkf_delivered(self, check_id):
-        self.write(check_id.id, {'state': 'delivered'})
-        return True
+    def default_date_in(self):
+        date_obj = Pool().get('ir.date')
+        return date_obj.today()
 
     def default_state(self):
         return 'draft'
+
+    @Workflow.transition('held')
+    def held(self, ids):
+        pass
+        
+    @ModelView.button
+    @Workflow.transition('deposited')
+    def deposited(self, ids):
+        pass
+
+    @Workflow.transition('delivered')
+    def delivered(self, ids):
+        pass
+
+    @ModelView.button
+    @Workflow.transition('rejected')
+    def rejected(self, ids):
+        pass
 
 AccountThirdCheck()
 
@@ -103,58 +183,24 @@ class AccountVoucherThirdCheck(ModelSQL):
 AccountVoucherThirdCheck()
 
 
-class VoucherCheck(ModelSQL, ModelView):
+class AccountVoucher(ModelSQL, ModelView):
     'Account Check Voucher'
     _name = 'account.voucher'
     _description = __doc__
 
     def amount_total(self, ids, name):
-        check_amount = 0
-        amount = super(VoucherCheck, self).amount_total(ids, name)
+        amount = super(AccountVoucher, self).amount_total(ids, name)
         for voucher in self.browse(ids):
             if voucher.third_check:
                 for t_check in voucher.third_check:
-                    check_amount += t_check.amount
+                    amount[voucher.id] += t_check.amount
             if voucher.issued_check:
                 for i_check in voucher.issued_check:
-                    check_amount += i_check.amount
+                    amount[voucher.id] += i_check.amount
             if voucher.third_pay_checks:
                 for check in voucher.third_pay_checks:
-                    check_amount += check.amount
-            amount[voucher.id] = amount[voucher.id] + check_amount
+                    amount[voucher.id] += check.amount
         return amount
-
-    def on_change_pay_amount_1(self, vals):
-        data = super(VoucherCheck, self).on_change_pay_amount_1(vals)
-        third_check_amount = 0
-        issued_check_amount = 0
-        third_pay_check_amount = 0
-        if vals.get('third_check'):
-            third_check_amount = self.check_amount(vals.get('third_check'))
-        if vals.get('issued_check'):
-            issued_check_amount = self.check_amount(vals.get('issued_check'))
-        if vals.get('third_pay_checks'):
-            third_pay_checks_amount = self.check_amount(
-                    vals.get('third_pay_checks'))
-        data['amount'] = data['amount'] + third_check_amount \
-                + issued_check_amount
-        return data
-
-    def on_change_pay_amount_2(self, vals):
-        data = super(VoucherCheck, self).on_change_pay_amount_2(vals)
-        third_check_amount = 0
-        issued_check_amount = 0
-        third_pay_check_amount = 0
-        if vals.get('third_check'):
-            third_check_amount = self.check_amount(vals.get('third_check'))
-        if vals.get('issued_check'):
-            issued_check_amount = self.check_amount(vals.get('issued_check'))
-        if vals.get('third_pay_checks'):
-            third_pay_checks_amount = self.check_amount(
-                    vals.get('third_pay_checks'))
-        data['amount'] = data['amount'] + third_check_amount \
-                + issued_check_amount
-        return data
 
     def amount_checks(self, ids, name):
         res = {}
@@ -178,48 +224,9 @@ class VoucherCheck(ModelSQL, ModelView):
             check_amount += check.get('amount')
         return check_amount
 
-    def on_change_third_check(self, vals):
-        res = {}
-        third_pay_check_amount = self.check_amount(
-                vals.get('third_pay_checks'))
-        third_check_amount = self.check_amount(vals.get('third_check'))
-        issued_check_amount = self.check_amount(vals.get('issued_check'))
-        res['total_checks'] = third_check_amount + issued_check_amount \
-                + third_pay_check_amount
-        res['amount'] = vals.get('pay_amount_1') + vals.get('pay_amount_2') \
-                + third_check_amount + issued_check_amount \
-                + third_pay_check_amount
-        return res
-
-    def on_change_issued_check(self, vals):
-        res = {}
-        third_pay_check_amount = self.check_amount(
-                vals.get('third_pay_checks'))
-        third_check_amount = self.check_amount(vals.get('third_check'))
-        issued_check_amount = self.check_amount(vals.get('issued_check'))
-        res['total_checks'] = third_check_amount + issued_check_amount \
-                + third_pay_check_amount
-        res['amount'] = vals.get('pay_amount_1') + vals.get('pay_amount_2') \
-                + third_check_amount + issued_check_amount \
-                + third_pay_check_amount
-        return res
-
-    def on_change_third_pay_checks(self, vals):
-        res = {}
-        third_pay_check_amount = self.check_amount(
-                vals.get('third_pay_checks'))
-        third_check_amount = self.check_amount(vals.get('third_check'))
-        issued_check_amount = self.check_amount(vals.get('issued_check'))
-        res['total_checks'] = third_check_amount + issued_check_amount \
-                + third_pay_check_amount
-        res['amount'] = vals.get('pay_amount_1') + vals.get('pay_amount_2') \
-                + third_check_amount + issued_check_amount \
-                + third_pay_check_amount
-        return res
-
     def prepare_moves(self, voucher_id):
-        pre_move = super(VoucherCheck, self).prepare_moves(voucher_id)
-        voucher = self.browse(voucher_id.id)
+        pre_move = super(AccountVoucher, self).prepare_moves(voucher_id)
+        voucher = self.browse(voucher_id)
         period_obj = Pool().get('account.period')
         if voucher.voucher_type == 'receipt':
             if voucher.third_check:
@@ -234,9 +241,9 @@ class VoucherCheck(ModelSQL, ModelView):
                     'name': voucher.number,
                     'debit': debit,
                     'credit': credit,
-                    'account': voucher.journal_id.third_check_account.id,
+                    'account': voucher.journal.third_check_account.id,
                     'move': pre_move.get('move_id'),
-                    'journal': voucher.journal_id.id,
+                    'journal': voucher.journal.id,
                     'period': period_obj.find(1, date=voucher.date),
                     'party': voucher.party.id,
                 })
@@ -252,9 +259,9 @@ class VoucherCheck(ModelSQL, ModelView):
                     'name': voucher.number,
                     'debit': debit,
                     'credit': credit,
-                    'account': voucher.journal_id.issued_check_account.id,
+                    'account': voucher.journal.issued_check_account.id,
                     'move': pre_move.get('move_id'),
-                    'journal': voucher.journal_id.id,
+                    'journal': voucher.journal.id,
                     'period': period_obj.find(1, date=voucher.date),
                     'party': voucher.party.id,
                 })
@@ -268,69 +275,69 @@ class VoucherCheck(ModelSQL, ModelView):
                     'name': voucher.number,
                     'debit': debit,
                     'credit': credit,
-                    'account': voucher.journal_id.third_check_account.id,
+                    'account': voucher.journal.third_check_account.id,
                     'move': pre_move.get('move_id'),
-                    'journal': voucher.journal_id.id,
+                    'journal': voucher.journal.id,
                     'period': period_obj.find(1, date=voucher.date),
                     'party': voucher.party.id,
                 })
 
         return pre_move
 
-    def action_paid(self, voucher_id):
-        super(VoucherCheck, self).action_paid(voucher_id)
-        voucher = self.browse(voucher_id.id)
+    @ModelView.button
+    def post(self, voucher_id):
+        super(AccountVoucher, self).post(voucher_id)
+        third_check_obj = Pool().get('account.third.check')
+        issued_check_obj = Pool().get('account.issued.check')
+        date_obj = Pool().get('ir.date')
+        
+        voucher = self.browse(voucher_id[0])
+        if voucher.issued_check:
+            check_ids = [x.id for x in voucher.issued_check]
+            issued_check_obj.write(check_ids, {
+                'receiving_party': voucher.party.id,
+            })
+            issued_check_obj.issued(check_ids)
         if voucher.third_check:
-            third_check_obj = Pool().get('account.third.check')
-            for check in voucher.third_check:
-                check.write(check.id, {'source_party_id': voucher.party.id})
-                third_check_obj.workflow_trigger_validate(check.id,
-                        'draft_cartera')
+            check_ids = [x.id for x in voucher.third_check]
+            third_check_obj.write(check_ids, {
+                'source_party': voucher.party.id,
+            })
+            third_check_obj.held(check_ids)
         if voucher.third_pay_checks:
-            third_check_obj = Pool().get('account.third.check')
-            for check in voucher.third_pay_checks:
-                check.write(check.id, {
-                    'destiny_party_id': voucher.party.id,
-                    'issued': True,
-                })
-                third_check_obj.workflow_trigger_validate(check.id,
-                        'cartera_delivered')
+            check_ids = [x.id for x in voucher.third_pay_checks]
+            third_check_obj.write(check_ids, {
+                'destiny_party': voucher.party.id,
+                'date_out': date_obj.today(),
+            })
+            third_check_obj.delivered(check_ids)
         return True
 
-    def __init__(self):
-        super(VoucherCheck, self).__init__()
-        self.pay_amount_1.on_change.extend(['issued_check',
-            'third_check', 'third_pay_checks'])
-        self.pay_amount_2.on_change.extend(['issued_check',
-            'third_check', 'third_pay_checks'])
-
-    issued_check = fields.One2Many('account.issued.check', 'voucher_id',
+    issued_check = fields.One2Many('account.issued.check', 'voucher',
         'Issued Checks',
-        on_change=['pay_amount_1', 'pay_amount_2',
-            'issued_check', 'third_check', 'third_pay_checks'],
         states={
             'invisible': Not(In(Eval('voucher_type'), ['payment'])),
+            'readonly': In(Eval('state'), ['posted']),
         })
 
     third_pay_checks = fields.Many2Many('account.voucher-account.third.check',
         'voucher', 'third_check', 'Third Checks',
-        on_change=['pay_amount_1', 'pay_amount_2', 'issued_check',
-            'third_check', 'third_pay_checks'],
+        domain=[('state', '=', 'held')],
         states={
             'invisible': Not(In(Eval('voucher_type'), ['payment'])),
+            'readonly': In(Eval('state'), ['posted']),
         })
 
-    third_check = fields.One2Many('account.third.check', 'voucher_id',
+    third_check = fields.One2Many('account.third.check', 'voucher_in',
         'Third Checks',
-        on_change=['pay_amount_1', 'pay_amount_2',
-            'third_check', 'issued_check', 'third_pay_checks'],
         states={
             'invisible': Not(In(Eval('voucher_type'), ['receipt'])),
+            'readonly': In(Eval('state'), ['posted']),
         })
 
     total_checks = fields.Function(fields.Float('Checks'), 'amount_checks')
 
-VoucherCheck()
+AccountVoucher()
 
 
 class JournalCheck(ModelSQL, ModelView):
