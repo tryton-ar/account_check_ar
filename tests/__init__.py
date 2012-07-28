90532a2bef679f88527d5d3274b14278792eceab
Sebastian Marro <smarro@thymbra.com>
2012-07-28 02:10:28 -0300

    Add tests

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+#This file is part of the account_check_ar module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+
+from test_account_check_ar import suite
