363f92ad3a3ca818f3fc55396bfdac72e4426635
Bruno M. Villasanti <bvillasanti@thymbra.com>
2014-02-08 18:41:35 -0300

    Fixes Development Status classifier

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(name='trytond_account_check_ar',
                 + info.get('translation', []),
     },
     classifiers=[
-        'Development Status :: 1 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
         'Framework :: Tryton',
         'Intended Audience :: Developers',
