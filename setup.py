622e9a8dfe8f3f04411c3d7fa09ccfed3eb2f1a4
Bruno M. Villasanti <bvillasanti@thymbra.com>
2014-02-10 20:41:48 -0300

    Use separate XML files for views

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='trytond_account_check_ar',
         ],
     package_data={
         'trytond.modules.account_check_ar': (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
         'Development Status :: 3 - Alpha',
