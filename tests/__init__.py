af2632d9285a7a0d6dfbfd3e5204cd5542ab31e0
Adrián Bernardi <adrian.bernardi@gmail.com>
2017-07-31 18:22:25 -0300

    Update Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
-#This file is part of the account_check_ar module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+try:
+    from trytond.modules.account_check_ar.tests.tests import suite
+except ImportError:
+    from .tests import suite
 
-from test_account_check_ar import suite
+__all__ = ['suite']
