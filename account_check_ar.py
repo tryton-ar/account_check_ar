2ddcdd5f7129d3bdcabeb9448e7248c1e2564b17
Luciano Rossi <lukio@gcoop.coop>
2019-07-29 17:52:05 -0300

    REL #6 #5 Corregimos implementacion estados: deposito -> revertir -> rechazar | depositar

--- account_check_ar.py
+++ account_check_ar.py
@@ -187,16 +187,18 @@ class AccountThirdCheck(ModelSQL, ModelView):
                 'invisible': Eval('state') != 'draft',
                 },
             'deposited': {
-                'invisible': Eval('state') != 'held',
+                'invisible': ~Eval('state').in_(['held', 'reverted']),
                 },
             'delivered': {
                 'invisible': Eval('state') != 'held',
                 },
             'rejected': {
+                'invisible': ~Eval('state') != 'reverted',
+                },
+            'reverted': {
                 'invisible': ~Eval('state').in_([
                     'deposited', 'delivered']),
                 },
-            'reverted': {},
             })
 
     @staticmethod
