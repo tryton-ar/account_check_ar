40cb06755d25455acffb4e2a3eb6674f1abddc17
Adrián Bernardi <adrian.bernardi@gmail.com>
2020-07-21 17:29:07 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ import re
 from configparser import ConfigParser
 from setuptools import setup
 
+MODULE2PREFIX = {
+    'account_voucher_ar': 'trytonar',
+    'bank_ar': 'trytonar',
+    }
+
 
 def read(fname):
     return io.open(
@@ -17,7 +22,12 @@ def read(fname):
 
 
 def get_require_version(name):
-    require = '%s >= %s.%s, < %s.%s'
+    if name in LINKS:
+        return '%s @ %s' % (name, LINKS[name])
+    if minor_version % 2:
+        require = '%s >= %s.%s.dev0, < %s.%s'
+    else:
+        require = '%s >= %s.%s, < %s.%s'
     require %= (name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
@@ -38,21 +48,25 @@ name = 'trytonar_account_check_ar'
 download_url = 'https://github.com/tryton-ar/account_check_ar/tree/%s.%s' % (
     major_version, minor_version)
 
+LINKS = {
+    'trytonar_account_voucher_ar': ('git+https://github.com/tryton-ar/'
+        'account_voucher_ar.git@%s.%s#egg=trytonar_account_voucher_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
+    'trytonar_bank_ar': ('git+https://github.com/tryton-ar/'
+        'bank_ar.git@%s.%s#egg=trytonar_bank_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
+    }
+
 requires = []
 for dep in info.get('depends', []):
-    if dep == 'bank_ar':
-        requires.append('trytonar_bank_ar @ git+https://github.com/tryton-ar/bank_ar.git@%s.%s#egg=trytonar_bank_ar-%s.%s' % (major_version, minor_version, major_version, minor_version))
-    elif dep == 'account_voucher_ar':
-        requires.append('trytonar_account_voucher_ar @ git+https://github.com/tryton-ar/account_voucher_ar.git@%s.%s#egg=trytonar_account_voucher_ar-%s.%s' % (major_version, minor_version, major_version, minor_version))
-    elif not re.match(r'(ir|res)(\W|$)', dep):
-        requires.append(get_require_version('trytond_%s' % dep))
+    if not re.match(r'(ir|res)(\W|$)', dep):
+        module_name = '%s_%s' % (MODULE2PREFIX.get(dep, 'trytond'), dep)
+        requires.append(get_require_version(module_name))
+
 requires.append(get_require_version('trytond'))
-requires.append('trytonar_party_ar @ git+https://github.com/tryton-ar/party_ar.git@%s.%s#egg=trytonar_party_ar-%s.%s' % (major_version, minor_version, major_version, minor_version))
-requires.append('pyafipws @ git+https://github.com/reingart/pyafipws.git@py3k#egg=pyafipws-py3k')
-requires.append('pysimplesoap @ git+https://github.com/pysimplesoap/pysimplesoap.git@stable_py3k#egg=pysimplesoap-stable_py3k')
 
 tests_require = [get_require_version('proteus')]
-dependency_links = []
+dependency_links = list(LINKS.values())
 
 setup(name=name,
     version=version,
@@ -67,31 +81,33 @@ setup(name=name,
         'trytond.modules.account_check_ar.tests',
         ],
     package_data={
-        'trytond.modules.account_check_ar': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
+        'trytond.modules.account_check_ar': (info.get('xml', []) + [
+            'tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later'
+        ' (GPLv3+)',
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     install_requires=requires,
     dependency_links=dependency_links,
     zip_safe=False,
